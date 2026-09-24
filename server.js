cd51b79984eb77fc1b6480ac03e15e8a43bf0323
royalteegee <aokwesirieze@gmail.com>
2026-09-24 13:16:53 +0100

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(cors({ origin: FRONTEND_URL }));
 app.use(express.json());
